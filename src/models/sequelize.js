a77e57592663f32409cf38765c3aa3e952dac8c1
Elias Eskelinen <elias.eskelinen@pm.me>
2022-03-28 08:49:19 +0300

    postgresdb for heroku

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -1,12 +1,25 @@
 const Sequelize = require('sequelize');
 
-// Initialize local database file
-const sequelize = new Sequelize({
-  logging: false,
-  dialect: 'sqlite',
-  // This path is relative to /backend folder unlike with require
-  storage: './db/database.sqlite',
-});
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+}
+else{
+  // Initialize local database file
+  sequelize = new Sequelize({
+    logging: false,
+    dialect: 'sqlite',
+    // This path is relative to /backend folder unlike with require
+    storage: './db/database.sqlite',
+  });
+}
 
 // Export the sequelize instance for use with models
 module.exports = sequelize;
